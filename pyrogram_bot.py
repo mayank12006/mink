bbbd16b21315f74899905ffe6afbd1fbf988db71
mayank12006 <mayank12006@gmail.com>
2025-07-22 15:03:34 +0530

    Add files via upload

--- pyrogram_bot.py
+++ pyrogram_bot.py
@@ -9,13 +9,12 @@ API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", "your_api_hash")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
 
-app = Client("mink_bot", api_id=API_ID, api_hash=API_HASH)
+app = Client("mink_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 async def start(client, message: Message):
-await message.reply("""🌟 Welcome to MinkBot V2
-This bot can download music from Spotify and YouTube!
-Enjoy 🎧""")
+    await message.reply("👋 Welcome to MinkBot V2 with Pyrogram!
+Send a voice message or audio file to identify music.")
 
 @app.on_message(filters.audio | filters.voice)
 async def handle_audio(client, message: Message):
