0e16a4736a1ee01c46ae0489a5d7d06bac2b39ff
mayank12006 <mayank12006@gmail.com>
2025-07-22 18:54:21 +0530

    Update pyrogram_bot.py

--- pyrogram_bot.py
+++ pyrogram_bot.py
@@ -6,16 +6,17 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 API_ID = int(os.environ.get("API_ID", 22080529))
-API_HASH = int(os.environ.get("API_HASH", 8c5ef7fe6857ea7aca95175ed2dcfbaa))
+API_HASH = os.environ.get("API_HASH", "8c5ef7fe6857ea7aca95175ed2dcfbaa")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7886843424:AAGuC-3JZC_80lwEzxfs_g8kPrybefOikac")
 
-app = Client("mink_bot", api_id=API_ID, bot_token=BOT_TOKEN)
+app = Client("minkadi_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 async def start(client, message: Message):
-   await message.reply("""🌟 Welcome to MinkBot V2
-This bot can download music from Spotify and YouTube!Enjoy 🎧""")
+    await message.reply("🌟 Welcome to MinkBot V2\n"
+        "This bot can download music from Spotify and YouTube! Enjoy 🎧")
     
+
 @app.on_message(filters.audio | filters.voice)
 async def handle_audio(client, message: Message):
     if message.audio or message.voice:
@@ -23,5 +24,4 @@ async def handle_audio(client, message: Message):
         await message.reply("🎶 Got your file! (music recognition logic goes here)")
         os.remove(file)
 
-app = Client("minkadi_bot")
 app.run()
