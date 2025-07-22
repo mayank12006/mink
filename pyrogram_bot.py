1a598b72137f57b8269d817952bd0d4637d2c970
mayank12006 <mayank12006@gmail.com>
2025-07-22 14:44:23 +0530

    Update pyrogram_bot.py

--- pyrogram_bot.py
+++ pyrogram_bot.py
@@ -9,7 +9,7 @@ API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", "your_api_hash")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
 
-app = Client("mink_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("mink_bot", api_id=API_ID, api_hash=API_HASH)
 
 @app.on_message(filters.command("start"))
 async def start(client, message: Message):
