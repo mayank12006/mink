51b6a13c9abf32ec164cd3f3c29c4f8f44b6288b
mayank12006 <mayank12006@gmail.com>
2025-07-22 15:32:21 +0530

    Update pyrogram_bot.py

--- pyrogram_bot.py
+++ pyrogram_bot.py
@@ -6,6 +6,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 API_ID = int(os.environ.get("API_ID", 22080529))
+API_HASH = int(os.environ.get("API_HASH", 8c5ef7fe6857ea7aca95175ed2dcfbaa))
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7886843424:AAGuC-3JZC_80lwEzxfs_g8kPrybefOikac")
 
 app = Client("mink_bot", api_id=API_ID, bot_token=BOT_TOKEN)
@@ -22,4 +23,5 @@ async def handle_audio(client, message: Message):
         await message.reply("🎶 Got your file! (music recognition logic goes here)")
         os.remove(file)
 
+app = Client("minkadi_bot")
 app.run()
